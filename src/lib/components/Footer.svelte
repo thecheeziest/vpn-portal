1722644e058b043578bd972ab3bde3d6429cf37a
followzoa <followzoa@gmail.com>
2024-01-25 16:51:44 +0900

    style: footer 고정 해제

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,7 +4,7 @@
     $: darkStyle = $darkmode ? "bg-base-300 text-base-content" : "bg-my-100 text-my-300"
 </script>
 
-<footer class={`footer footer-center p-4 fixed bottom-0 ${darkStyle}`}>
+<footer class={`footer footer-center p-4 absolute bottom-0 ${darkStyle}`}>
     <aside>
         <p>Copyright © 2023 duruan</p>
     </aside>
